f9230fcc618dc1fb42e58f4c21b35de2aff568de
jobayer-h <jobayer.dev@gmail.com>
2021-03-03 23:16:19 +0600

    Functional Web App

--- src/Components/Dashbord/Sidebar.js
+++ src/Components/Dashbord/Sidebar.js
@@ -1,26 +1,53 @@
 import { faCommentDots, faList, faPlus, faShoppingCart, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { userContext } from '../../App';
 import logo from './../../resorces/logos/logo.png'
 import './Dashbord.css'
 const Sidebar = () => {
+    const [logedInUser, setLogedInUser] = useContext(userContext);
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/isAdmin', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ email: logedInUser.email })
+        })
+            .then(res => res.json())
+            .then(data => setIsAdmin(data))
+    }, [])
+
+
+
+
+
     return (
         <div>
             <div className="sidebar-logo">
-                <Link to="/"><img src={logo} alt=""/></Link>
+                <Link to="/"><img src={logo} alt="" /></Link>
             </div>
-            <br/><br/><br/>
+            <br /><br /><br />
             <div className="side-menu">
                 <ul>
-                    <li><Link className="link" to='/dashbord/order'><FontAwesomeIcon icon={faShoppingCart}/> Order</Link></li>
-                    <li><Link className="link" to='/dashbord/service'><FontAwesomeIcon icon={faList}/> Service List</Link></li>
-                    <li><Link className="link" to='/dashbord/review'><FontAwesomeIcon icon={faCommentDots}/> Review</Link></li>
 
-                    <li><Link className="link" to='/dashbord/allservice'><FontAwesomeIcon icon={faList}/> Service List</Link></li>
-                    <li><Link className="link" to='/dashbord/addservice'><FontAwesomeIcon icon={faPlus}/> Add Service</Link></li>
-                    <li><Link className="link" to='/dashbord/addadmin'><FontAwesomeIcon icon={faUserPlus}/> Make Admin</Link></li>
-                    
+                    {
+                        isAdmin ?
+                            <div>
+                                <li><Link className="link" to='/dashbord/allservice'><FontAwesomeIcon icon={faList} /> Service List</Link></li>
+                                <li><Link className="link" to='/dashbord/addservice'><FontAwesomeIcon icon={faPlus} /> Add Service</Link></li>
+                                <li><Link className="link" to='/dashbord/addadmin'><FontAwesomeIcon icon={faUserPlus} /> Make Admin</Link></li>
+                            </div>
+                            :
+                            <div>
+                                <li><Link className="link" to='/dashbord/order'><FontAwesomeIcon icon={faShoppingCart} /> Order</Link></li>
+                                <li><Link className="link" to='/dashbord/service'><FontAwesomeIcon icon={faList} /> Service List</Link></li>
+                                <li><Link className="link" to='/dashbord/review'><FontAwesomeIcon icon={faCommentDots} /> Review</Link></li>
+                            </div>
+                    }
+
+
                 </ul>
             </div>
         </div>
